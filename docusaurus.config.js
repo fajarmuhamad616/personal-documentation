4840fdf896acbfcfd3fab4d6d79c61298a5052d8
Fajar M Al-Hijri <fajarmuhamad616@gmail.com>
2025-01-29 00:33:49 +0700

    Add to remote editUrl and fix path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,8 +45,8 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         // blog: {
         //   showReadingTime: true,
@@ -113,7 +113,7 @@ const config = {
             items: [
               {
                 label: 'PostgreSQL',
-                to: '/docs/postgresql/introduction',
+                to: '/docs/PostgreSQL/introduction',
               },
             ],
           },
